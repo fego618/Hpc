ae754bbb5fb91a697f40a7a516e9135c34c5d37a
UTP <utp@CRIE.LOCAL>
2016-10-07 14:12:56 -0500

    convolucion 2D CPU ok

--- Convolucion/convolucion2D.c
+++ Convolucion/convolucion2D.c
@@ -24,23 +24,21 @@ __global__ void  convolutionGPUkernel_2D(int *M, int *mascara,int *resultado,int
 }
 
 void  convolutionCPU2D(int *M, int *mascara,int *resultado,int m, int n, int widthM){
-  int j = blockIdx.x*blockDim.x + threadIdx.x;
-  int i = blockIdx.y*blockDim.y + threadIdx.y;
-  for(int i = 0; i<m;i++){
-    for(int j=0; j<n ; j++){
+  for(int row = 0;row<m;row++){
+    for(int col=0;col<n;col++){
       int p = 0;
-      int start_j = j - (widthM/2);
-      int start_i = i - (widthM/2);
+      int start_col = col - (widthM/2);
+      int start_row = row - (widthM/2);
       for (int i = 0; i < widthM ; i++) {
         for (int j = 0; j < widthM; j++) {
-          int curRow = start_i + i;
-          int curCol = start_j + j;
+          int curRow = start_row + i;
+          int curCol = start_col + j;
           if(curRow > -1 && curRow < m && curCol > -1 && curCol < n){
             p += M[curRow*m + curCol]*mascara[i*widthM + j];
           }
         }
       }
-      resultado[row*n + j] = p;
+      resultado[row*n + col] = p;
     }
   }
 }
@@ -72,19 +70,24 @@ int printData(int *Mat, int m,int n, int tipo){
 
 int main()
 {
-	int *h_n,*h_mascara,*d_n,*d_mascara,*d_r,*h_result;
-  clock_t startGPU, endGPU;
-  double gpu_time_used;
-	// dimensiones
-	int Mm=7, Mn= 7, mascaraWidth = 5;
+  int *h_n,*h_mascara,*h_r,*d_n,*d_mascara,*d_r,*h_result;
+  clock_t startGPU, endGPU,startCPU, endCPU;
+  double gpu_time_used,cpu_time_used;
+  // dimensiones
+  int Mm=7, Mn= 7, mascaraWidth = 5;
 
-	// asignacion de memoria en el host , matrices de Mm*Mn , mascaraM*mascaraN
-	h_n= (int*)malloc((Mm*Mn)*sizeof(int));
-	h_mascara= (int*)malloc((mascaraWidth*mascaraWidth)*sizeof(int));
+  // asignacion de memoria en el host , matrices de Mm*Mn , mascaraM*mascaraN
+  h_n= (int*)malloc((Mm*Mn)*sizeof(int));
+  h_mascara= (int*)malloc((mascaraWidth*mascaraWidth)*sizeof(int));
   h_result= (int*)malloc((Mm*Mn)*sizeof(int));
-	//inicializacion
-	inicializarMat(h_n,Mm,Mn);
-	inicializarMat(h_mascara,Mm,Mn);
+  h_r= (int*)malloc((Mm*Mn)*sizeof(int));
+  //inicializacion
+  inicializarMat(h_n,Mm,Mn);
+  inicializarMat(h_mascara,Mm,Mn);
+  startCPU = clock();
+  convolutionCPU2D(h_n,h_mascara,h_r,Mm,Mn,mascaraWidth);
+  endCPU = clock();
+  cpu_time_used = ((double) (endCPU - startCPU)) / CLOCKS_PER_SEC;
 
   cudaMalloc((void**)&d_n,(Mm*Mn)*sizeof(int));
   cudaMalloc((void**)&d_mascara,(mascaraWidth*mascaraWidth)*sizeof(int));
@@ -101,13 +104,15 @@ int main()
 
   cudaMemcpy(h_result,d_r,(Mm*Mn)*sizeof(int),cudaMemcpyDeviceToHost);
   endGPU = clock();
-	printData(h_n,Mm,Mn,1);
-	printData(h_mascara,mascaraWidth,mascaraWidth,2);
+  printData(h_n,Mm,Mn,1);
+  printData(h_mascara,mascaraWidth,mascaraWidth,2);
+  printData(h_r,Mm,Mn,3);
   printData(h_result,Mm,Mn,3);
 
   gpu_time_used = ((double) (endGPU - startGPU)) / CLOCKS_PER_SEC;
+  printf("Tiempo CPU : %.10f\n", cpu_time_used);
   printf("Tiempo GPU : %.10f\n", gpu_time_used);
 
 
-	return 0;
+  return 0;
 }
