e400b42ef4a016db6b8d67806afe4e1a1b746b09
faesgonzalez <fego618@gmail.com>
2016-11-22 10:52:19 -0500

    correccion .sh

--- parcial2/compile.sh
+++ parcial2/compile.sh
@@ -1,3 +1,3 @@
-mpicc -c mpi_mult_matriz.c -o mpi_mult_matriz.o
-/usr/local/cuda/bin/nvcc cuda_mult_matriz.cu -c cuda_mult_matriz.o
-mpicc mpi_mult_matriz.o cuda_mult_matriz.o -o mpiWithCuda_mm -L/usr/local/cuda/lib64/ -lcudart
+mpic++ -c mpi_mult_matriz.c -o mpi_mult_matriz.o
+/usr/local/cuda/bin/nvcc -c cuda_mult_matriz.cu -o cuda_mult_matriz.o -Wno-deprecated-gpu-targets
+mpic++ mpi_mult_matriz.o cuda_mult_matriz.o -o mpiWithCuda_mm -L/usr/local/cuda/lib64/ -lcudart
